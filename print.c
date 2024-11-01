89bbe58b41206742ea471e0363e0a4e1df88df5c
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-11-01 18:40:09 +0600

    first commit

---
+++ print.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    printf("hello world\tI am learning C programming");
+    // printf("I am learning c \\");
+    return 0;
+}
---
+++ variable.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int myAge=10;
+    printf("%d", myAge);
+
+    return 0;
+}
