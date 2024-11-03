803b28af60788c2f77faacd16db9ac67dc2c26dd
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-11-03 14:59:25 +0600

    problem with c has been expolred

---
+++ day-2.5/problem_1.c
@@ -0,0 +1,20 @@
+// You need to print the lines given below in the same format as it is -
+
+// “Recently I heard that you’ve achieved 95% marks in your exam. 
+// This is brilliant! 
+// I wish you’ll shine in your life!	Good luck with all the barriers(/\) in your life.”
+
+#include<stdio.h>
+int main(){
+    printf("Recently I heard that you’ve achieved 95% marks in your exam.\nThis is brilliant!\nI wish you’ll shine in your life!\nGood luck with all the barriers(/\\) in your life. \n");
+    return 0;
+}
+
+
+// Sample Input
+// Sample Output
+// There is no input
+// Recently I heard that you’ve achieved 95% marks in your exam. 
+// This is brilliant! 
+// I wish you’ll shine in your life!	Good luck with all the barriers(/\) in your life.
+
