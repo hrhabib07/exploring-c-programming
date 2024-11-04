674438d29a30159573704c43e26688e8e5ebea80
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-11-04 06:56:44 +0600

    exporing problem solving

---
+++ day-2.5/problem_2.c
@@ -0,0 +1,21 @@
+// You need to take two integer values as input and show the summation, subtraction, multiplication and division in the given format below.
+
+#include<stdio.h>
+int main(){
+
+    int num1;
+    scanf("num1 %d",&num1);
+    printf("%d",num1);
+
+    return 0;
+}
+
+
+
+// For example:
+// Inputs are 5 and 2
+// Then you’ll give output as:
+// 5 + 2 = 7
+// 5 - 2 = 3
+// 5 * 2 = 10
+// 5 / 2 = 2.50
