b2213da352769e3334a5d39fa2d020381206e01c
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-12-04 10:06:27 +0600

    loop sum exporled

---
+++ day-3/loop_sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    long long int sum = 0;
+    int n;
+    int i;
+
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++) {
+        sum = sum + i;
+    }
+    printf("%lld", sum);
+    return 0;
+}
