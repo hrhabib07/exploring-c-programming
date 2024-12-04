bd44a226ecbf34099f2f7c641692fa4edf231e85
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-12-04 10:55:53 +0600

    do while has been explred

---
+++ day-3/while.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int i = 1;
+
+    // for(i=1; i<=10; i++){
+    //     printf("%d \n",i);
+    // }
+    while(i<=10){
+        printf("%d \n", i);
+        i++;
+    }
+    return 0;
+}
