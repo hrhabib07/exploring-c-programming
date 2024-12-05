55d6cb536c2d20865385bc8f76f3e9ae7bcb691f
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-12-05 10:32:32 +0600

    miltiplication table related problem has been solved

--- day-4/E_Max.c
+++ day-4/E_Max.c
@@ -3,7 +3,7 @@ int main(){
     int n;
     int max=0;
     scanf("%d", &n);
-   for (int i = 0; i < n; i++)
+   for (int i = 0; n>=1 && i < n && n<=1000; i++)
    {
     int current;
     scanf("%d", &current);
